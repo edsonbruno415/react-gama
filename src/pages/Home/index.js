156d3c3d1a1b5eb02b58c69c47840721de441000
Edson Bruno <edsonbruno415@gmail.com>
2020-05-25 23:23:29 -0300

    :tada: initial commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
+import * as S from './styled';
+
+async function findRepositories(user) {
+  const repos = await axios.get('https://api.github.com/users/edsonbruno415/repos');
+  localStorage.setItem('repositorios', JSON.stringify(repos.data));
+}
+
+function Home() {
+  const [user, setUser] = useState('');
+  return (
+    <S.Container>
+      <S.Input value={user} className="usuarioInput" placeholder="Usuário" onChange={e => setUser(e.target.value)} />
+      <S.Button onClick={e => findRepositories('user')}>Procurar</S.Button>
+    </S.Container>
+  );
+}
+
+export default Home;
---
+++ src/pages/Repositories/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import * as S from './styled';
+
+function Repositories() {
+    return (
+        <S.Container>
+        <S.Title>Repositorios</S.Title>
+        <S.List>
+            <S.ListItem>Repositorio</S.ListItem>
+        </S.List>
+        </S.Container>
+    );
+}
+
+export default Repositories;
